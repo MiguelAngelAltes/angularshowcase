dcb340fbde200666d68d54f84982df9ca700c6ba
java500 <java500@SINENSIA.EDU>
2020-02-27 18:49:08 +0100

    se añade nuevo componente box y formsmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// decorador
+import { NgModule }             from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { CalculatorComponent } from './components/calculator/calculator.component';
-import { RelojComponent } from './components/reloj/reloj.component';
+// módulos
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
+
+// componentes
+import { AppComponent }         from './app.component';
+import { CalculatorComponent }  from './components/calculator/calculator.component';
+import { RelojComponent }       from './components/reloj/reloj.component';
+import { BoxComponent }         from './components/box/box.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CalculatorComponent,
-    RelojComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, CalculatorComponent,RelojComponent, BoxComponent],
+  imports:      [BrowserModule, FormsModule],
+  providers:    [],
+  bootstrap:    [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/box/box.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-box',
+    templateUrl: './box.component.html',
+    styleUrls:['./box.component.css']
+})
+export class BoxComponent {
+
+    titulo = "Componente Box";
+
+
+
+
+}
