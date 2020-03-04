969c496531c1656a3fbfec34e2b3606cb62994bf
java503 <java503@SINENSIA.EDU>
2020-03-04 20:51:40 +0100

    Añado un titulo nuevo en material3

--- src/app/components/material3/material3.component.ts
+++ src/app/components/material3/material3.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-material3',
   templateUrl: './material3.component.html',
   styleUrls: ['./material3.component.css']
 })
-export class Material3Component implements OnInit {
+export class Material3Component {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  
+  
 
 }
