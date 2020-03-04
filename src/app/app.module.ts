8dd4d55ba5c0e3adb84197a8f9a02acf223a6fb1
java500 <java500@SINENSIA.EDU>
2020-03-04 18:31:12 +0100

    se añade componente material1 con tabla de ejemplo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule }                       from '@angular/core';
 // módulos
 import { BrowserModule }                  from '@angular/platform-browser';
 import { FormsModule }                    from '@angular/forms';
+import { MatTableModule }                 from '@angular/material/table';
+import { BrowserAnimationsModule }        from '@angular/platform-browser/animations';
 
 // componentes
 import { AppComponent }                   from './app.component';
@@ -11,21 +13,24 @@ import { CalculatorComponent }            from './components/calculator/calculat
 import { RelojComponent }                 from './components/reloj/reloj.component';
 import { BoxComponent }                   from './components/box/box.component';
 import { TemperaturaConverterComponent }  from './components/temperatura-converter/temperatura-converter.component';
+import { ConversorDivisasComponent }      from './components/conversor-divisas/conversor-divisas.component';
+import { PipeShowcaseComponent }          from './components/pipe-showcase/pipe-showcase.component';
+import { List1Component }                 from './components/list1/list1.component';
+import { List2Component }                 from './components/list2/list2.component';
+import { VendingMachineComponent }        from './components/vending-machine/vending-machine.component';
 
 // services
 import { ConversorTemperaturaService }    from './services/conversor-temperatura.service';
-import { ConversorDivisasComponent } from './components/conversor-divisas/conversor-divisas.component';
-import { PipeShowcaseComponent } from './components/pipe-showcase/pipe-showcase.component';
-import { CharCounterPipe } from './pipes/char-counter.pipe';
-import { ReversePipe } from './pipes/reverse.pipe';
-import { TrimestrePipe } from './pipes/trimestre.pipe';
-import { List1Component } from './components/list1/list1.component';
-import { List2Component } from './components/list2/list2.component';
-import { VendingMachineComponent } from './components/vending-machine/vending-machine.component';
+
+// pipes
+import { CharCounterPipe }                from './pipes/char-counter.pipe';
+import { ReversePipe }                    from './pipes/reverse.pipe';
+import { TrimestrePipe }                  from './pipes/trimestre.pipe';
+import { Material1Component } from './components/material1/material1.component';
 
 @NgModule({
-  declarations: [AppComponent, CalculatorComponent,RelojComponent, BoxComponent, TemperaturaConverterComponent, ConversorDivisasComponent, PipeShowcaseComponent, CharCounterPipe, ReversePipe, TrimestrePipe, List1Component, List2Component, VendingMachineComponent],
-  imports:      [BrowserModule, FormsModule],
+  declarations: [AppComponent, CalculatorComponent,RelojComponent, BoxComponent, TemperaturaConverterComponent, ConversorDivisasComponent, PipeShowcaseComponent, CharCounterPipe, ReversePipe, TrimestrePipe, List1Component, List2Component, VendingMachineComponent, Material1Component],
+  imports:      [BrowserModule, FormsModule, BrowserAnimationsModule, MatTableModule],
   providers:    [ConversorTemperaturaService],
   bootstrap:    [AppComponent]
 })
